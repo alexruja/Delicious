cd716eb949a21f8673653c0d7129781ed7c4e962
Cristian-Alexandru Ruja <christianajur@gmail.com>
2022-08-10 23:18:35 +0300

    Pre-bug fix, new damn bug added -_-

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import { BrowserRouter as Router } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Search></Search>
         <Category></Category>
         <Pages></Pages>
       </Router>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Route, Routes} from "react-router-dom";
 //import { filterProps } from "framer-motion";
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 
 function Pages() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="cuisine/:type" element={<Cuisine />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
       </Routes>
   );
 }
